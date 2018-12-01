46ad8a47e902449cd064822a89a037e2d3085a65
Etga İsmail ERDEM <45507641+EtgaErdem7@users.noreply.github.com>
2018-12-02 02:20:14 +0300

    Create InputAPI.java

---
+++ InputAPI.java
@@ -0,0 +1,48 @@
+package App;
+
+public class InputAPI {
+	
+	private int trafficRateGoogle;
+	private int remTimeGoogle;
+	private String trafficReasonMercedes;
+	private int trafficRateMercedes;
+	private String failedEqp;
+	
+	
+	
+	
+	public int getTrafficRateGoogle() {
+		return trafficRateGoogle;
+	}
+	public void setTrafficRateGoogle(int trafficRateGoogle) {
+		this.trafficRateGoogle = trafficRateGoogle;
+	}
+	public int getRemTimeGoogle() {
+		return remTimeGoogle;
+	}
+	public void setRemTimeGoogle(int remTimeGoogle) {
+		this.remTimeGoogle = remTimeGoogle;
+	}
+	public String getTrafficReasonMercedes() {
+		return trafficReasonMercedes;
+	}
+	public void setTrafficReasonMercedes(String trafficReasonMercedes) {
+		this.trafficReasonMercedes = trafficReasonMercedes;
+	}
+	public int getTrafficRateMercedes() {
+		return trafficRateMercedes;
+	}
+	public void setTrafficRateMercedes(int trafficRateMercedes) {
+		this.trafficRateMercedes = trafficRateMercedes;
+	}
+	public String getFailedEqp() {
+		return failedEqp;
+	}
+	public void setFailedEqp(String failedEqp) {
+		this.failedEqp = failedEqp;
+	}
+	
+	
+	
+
+}
