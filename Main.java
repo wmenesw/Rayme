5e11ba42ed7bbd84dde5a2a42d3335a1d671213b
Etga İsmail ERDEM <45507641+EtgaErdem7@users.noreply.github.com>
2018-12-02 02:19:11 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+package App;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		InputUsr input = new InputUsr("Paris", "Istanbul");
+		input.setCompansation(1000);
+		input.setCompansation(10);
+		
+		LinkedList<Road> roads = new LinkedList<Road>();
+		
+		roads.add(new Road());
+		roads.get(0).setDistance(1000); //Istanbul - Paris is 1000km.
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
